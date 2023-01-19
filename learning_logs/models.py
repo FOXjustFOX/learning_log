5abe2a3f9b12289744f62542e89f3df9b565b642
FOXjustFOX <iglis.com20@gmail.com>
2023-01-19 13:18:15 +0100

    commit

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Topic(models.Model):
     """toppic defined by user"""
     text = models.CharField(max_length=100)
-    desc = models.CharField(max_length=250, default="")
+    desc = models.CharField(max_length=250, default="Description")
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -16,13 +16,12 @@ class Topic(models.Model):
 class Entry(models.Model):
     """exact info about progress in learning"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    text = models.TextField(max_length=100)
-    desc = models.TextField(default="")
-    date_added = models.DateTimeField(auto_now=True)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = 'entries'
 
         
-    def __str__(self):
+    def __str__(self) -> str:
         return self.text[:50] if len(self.text) > 50 else self.text
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,64 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Topic
-from .forms import TopicForm, EntryForm
 
 # Create your views here.
-
 
 def index(request):
     """main page for the learning_logs app"""
     return render(request, 'learning_logs/index.html')
 
-
 def topics(request):
     """render all of the topics"""
     topics = Topic.objects.order_by('date_added')
-    count = len(topics)
-    context = {'topics': topics, 'count':count}
+    context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
-
-
-def topic(request, topic_id):
-    """render the details about a specific topic"""
-    topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('-date_added')
-    count = len(entries)
-    context = {'topic': topic, 'entries': entries, 'count': count}
-    return render(request, 'learning_logs/topic.html', context)
-
-
-def new_topic(request):
-    """add new topic"""
-    if request.method != 'POST':
-        # the user did not provide any data
-        form = TopicForm()
-    else:
-        # the user did provide some data
-        form = TopicForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('learning_logs:topics')
-
-    context = {'form': form}
-    return render(request, 'learning_logs/new_topic.html', context)
-
-
-def new_entry(request, topic_id):
-    """Dodanie nowego wpisu dla określonego tematu."""
-    topic = Topic.objects.get(id=topic_id) 
-    if request.method != 'POST': 
-        # Nie przekazano żadnych danych, należy utworzyć pusty formularz.
-        form = EntryForm() 
-    else:
-        # Przekazano dane za pomocą żądania POST, należy je przetworzyć.
-        form = EntryForm(data=request.POST) 
-
-    if form.is_valid():
-        new_entry = form.save(commit=False) 
-        new_entry.topic = topic 
-        new_entry.save()
-        return redirect('learning_logs:topic', topic_id=topic_id) 
-    # Wyświetlenie pustego formularza.
-
-    context = {'topic': topic, 'form': form}
-    return render(request, 'learning_logs/new_entry.html', context)
