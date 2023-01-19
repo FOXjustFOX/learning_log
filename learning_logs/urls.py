5759347e8714bbdf9100244375b1f960de9753c1
FOXjustFOX <iglis.com20@gmail.com>
2023-01-19 13:08:20 +0100

    init and more doing than the other time

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+
+"""Definiuje wzorce adresów URL dla learning_logs.""" 
+from django.urls import path 
+from . import views 
+
+
+app_name = 'learning_logs' 
+
+urlpatterns = [
+ # Strona główna.
+ path('', views.index, name='index'),
+ path('topics/', views.topics, name='topics'),
+]
